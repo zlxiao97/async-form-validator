4a72a6f38a3d267ee6117e6f1e944af021b88c21
TadXiao <zlxiao97@yahoo.com>
2022-06-03 12:00:52 +0800

    feat: 异步自定义校验

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <a-form :form="form" @submit="handleSubmit">
+    <a-form-item label="note">
+      <a-input
+        v-decorator="[
+          'note',
+          {
+            rules: [
+              { required: true, message: 'Please input your note!' },
+              {
+                max: 10,
+                message: 'Please enter a note with a maximum length of 10',
+              },
+              {
+                validator,
+              },
+            ],
+          },
+        ]"
+      />
+    </a-form-item>
     <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
       <a-button type="primary" html-type="submit"> Submit </a-button>
     </a-form-item>
@@ -14,11 +33,27 @@ export default {
   },
   beforeCreate() {
     // 初始化表单实例，绑定 自定义 onChange 事件
-    this.form = this.$form.createForm(this, {
-      onValuesChange: () => {}
-    });
+    this.form = this.$form.createForm(this);
   },
-  methods: {}
+  methods: {
+    validator(rule, value, callback) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (String(value).startsWith("A")) {
+            callback();
+            resolve();
+          } else {
+            callback("自定义校验：请使用字母A作为开头");
+            reject("自定义校验：请使用字母A作为开头");
+          }
+        }, 100);
+      });
+    },
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields();
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Antd from "ant-design-vue";
-import DcForm from "./components/DcForm";
 import "ant-design-vue/dist/antd.css";
 
 Vue.use(Antd);
-Vue.component("DcForm", DcForm);
 
 Vue.config.productionTip = false;
 
